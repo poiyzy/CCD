e90a02d14100352bca8b6992bfcea22884925e82
Roy Young <zhuoyuyang@gmail.com>
2012-01-18 11:00:46 +0800

    在post中加入title验证 title必须不为空

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,7 @@ class Post < ActiveRecord::Base
   default_scope :order => 'created_at DESC'
   
   validates :content, :presence => true
+  validates :title, :presence => true
   
   def video_share
     @videoshare
